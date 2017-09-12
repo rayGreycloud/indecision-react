943129610e357b5ac522e59c7476e79ba93c4c63
rayGreycloud <rayGreycloud@gmail.com>
2017-09-12 12:31:48 -0700

    Exploring jsx with Andrew

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,39 @@
 console.log('App is running...');
 
 var template = React.createElement(
-  'h2',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h2',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Can\'t decide? Let the app decide for you...'
+  )
+);
+
+var templateTwo = React.createElement(
+  'div',
+  { className: 'container' },
+  React.createElement(
+    'h1',
+    null,
+    'Anakin Skywalker'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 10'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Tantooine'
+  )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 console.log('App is running...');
 
-var template = <h2>Indecision App</h2>;
+var template = (
+  <div>
+    <h2>Indecision App</h2>
+    <p>Can't decide? Let the app decide for you...</p>
+  </div>
+);
+
+var templateTwo = (
+  <div className="container">
+    <h1>Anakin Skywalker</h1>
+    <p>Age: 10</p>
+    <p>Location: Tantooine</p>
+  </div>
+)
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
