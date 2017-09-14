0a98d01530ad753b165a9081e477df6248642f54
rayGreycloud <rayGreycloud@gmail.com>
2017-09-14 09:24:12 -0700

    Challenge - make counter buttons functional

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -31,43 +31,48 @@ var template = React.createElement(
 var count = 0;
 var addOne = function addOne() {
   count++;
-  console.log('Count: ' + count);
+  renderCounterApp();
 };
 var minusOne = function minusOne() {
   count--;
-  console.log('Count: ' + count);
+  renderCounterApp();
 };
 var reset = function reset() {
   count = 0;
-  console.log('Count: ' + count);
+  renderCounterApp();
 };
-
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Count: ',
-    count
-  ),
-  React.createElement(
-    'button',
-    { onClick: addOne },
-    '+1'
-  ),
-  React.createElement(
-    'button',
-    { onClick: minusOne },
-    '-1'
-  ),
-  React.createElement(
-    'button',
-    { onClick: reset },
-    'reset'
-  )
-);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'reset'
+    )
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -17,26 +17,31 @@ const template = (
 let count = 0;
 const addOne = () => {
   count++;
-  console.log(`Count: ${count}`);
+  renderCounterApp();
 };
 const minusOne = () => {
   count--;
-  console.log(`Count: ${count}`);
+  renderCounterApp();
 };
 const reset = () => {
   count = 0;
-  console.log(`Count: ${count}`);
+  renderCounterApp();
 };
-
-const templateTwo = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={addOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>reset</button>
-  </div>
-);
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp = () => {
+
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
