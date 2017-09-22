e6aa0ce00ee70936057450e76e1cc5318d2afc26
rayGreycloud <rayGreycloud@gmail.com>
2017-09-21 19:18:36 -0700

    Add style variables, container and header styles

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -74,18 +74,21 @@ class IndecisionApp extends React.Component {
           title={this.state.title}
           subtitle={this.state.subtitle}
         />
-        <Action
-          hasOptions={this.state.options.length> 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length> 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+        </div>
+
         <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
